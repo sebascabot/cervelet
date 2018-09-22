b7f36bde86dc728234a6c1948428160501ab838f
Sébastien Cabot <sebascabot@gmail.com>
2018-09-22 11:55:03 -0400

    Nice command line to broadcase UDP

---
+++ nodejs/tools/send-udp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+HOST="255.255.255.255"
+PORT="41234"
+
+JSON='{"rgb":[10,0,0]}'
+JSON='{"from":"owl","to":"hub","button":1}'
+JSON='{"from":"hub","to":"owl","red": 1023,"green":0,"blue":0}'
+JSON='{"from":"hub","to":"owl","red": 0,"green":0,"blue":1023}'
+JSON='{"from":"rfid","to":"hub","rfid":[246,40,16,75]}' # RED Tag
+
+echo -n "$JSON" | socat - UDP-DATAGRAM:255.255.255.255:41234,broadcast
+
+# Nice Bash feature is a clever `/dev/upd` device,
+# but there's a flaw, it does not allow to broadcast.
+# HOST="cervelet.local"
+# echo -n "$JSON" >/dev/udp/${HOST}/${PORT}
+
