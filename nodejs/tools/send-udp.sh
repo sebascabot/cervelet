e500fcc6859be18d666360066d709dd7b4ce5284
Sébastien Cabot <sebascabot@gmail.com>
2018-09-26 00:07:47 -0400

    New device eqbe 5x5
    Add speed for gHue
    Use JSON to select pattern, hue, brightness

--- nodejs/tools/send-udp.sh
+++ nodejs/tools/send-udp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HOST="255.255.255.255"
+HOST="10.0.50.255"
 PORT="41234"
 
 JSON='{"rgb":[10,0,0]}'
@@ -9,7 +9,9 @@ JSON='{"from":"hub","to":"owl","red": 1023,"green":0,"blue":0}'
 JSON='{"from":"hub","to":"owl","red": 0,"green":0,"blue":1023}'
 JSON='{"from":"rfid","to":"hub","rfid":"A5:11:F8:43"}' # RED Tag
 
-echo -n "$JSON" | socat - UDP-DATAGRAM:255.255.255.255:41234,broadcast
+echo -n "$JSON" | socat - UDP-DATAGRAM:${HOST}:${PORT},broadcast
+echo -n '{"to":"eqbe5","pattern":"rainbow"}' | socat - UDP-DATAGRAM:10.0.50.255:41234,broadcast
+echo -n '{"to":"eqbe5","pattern":"solid"}' | socat - UDP-DATAGRAM:10.0.50.255:41234,broadcast
 
 # Nice Bash feature is a clever `/dev/upd` device,
 # but there's a flaw, it does not allow to broadcast.
